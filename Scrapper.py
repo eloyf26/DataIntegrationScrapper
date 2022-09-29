524cf919d207d963fd9ea373753809fe6fa292f6
LucyGaTo <gallegotorreslucia@gmail.com>
2022-09-29 22:40:32 +0200

    Pruebas1

--- Scrapper.py
+++ Scrapper.py
@@ -2,6 +2,9 @@ from cgi import test
 from bs4 import BeautifulSoup
 import requests
 def test():
+    #cArray = 
+    #for linkCinema in cArray:
+        #page = requests.get(linkCinema)
     page = requests.get("https://cine.entradas.com/cine/madrid/yelmo-cines-ideal/shows/movies")
 
     if (page.status_code != 200):
@@ -12,5 +15,13 @@ def test():
     
     soup=BeautifulSoup(page.content,'html.parser')
     print(soup.prettify())
+
+    #print ("Aqui llego")
+    #gf = soup.find_all("movie_image")
+    #print ("Aqui llego")
+    #for i in gf:
+    #    print ("Aqui llego")
+    #    print(i)
+
 
 test()
