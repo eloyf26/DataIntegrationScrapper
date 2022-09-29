cb4e48ae28eb61d0dc1d073cd2b54f08ebf733b8
Eloyf26 <eloyf26@gmail.com>
2022-09-29 07:58:23 -0700

    GetMovieTheaterLinks

--- Scrapper.py
+++ Scrapper.py
@@ -1,5 +1,6 @@
 from cgi import test
 from bs4 import BeautifulSoup
+import pandas as pd
 import requests
 def test():
     page = requests.get("https://cine.entradas.com/cine/madrid/yelmo-cines-ideal/shows/movies")
@@ -10,9 +11,17 @@ def test():
         exit
     
     soup=BeautifulSoup(page.content,'html.parser')
-    print(soup.prettify())
+    print(page.content)
  
-def GetMovieTheaters():
-        
+def GetMovieTheatersLink():
+    # Load the xlsx file
+    excel_data = pd.read_excel(r"C:\Users\eloyf\Downloads\Coordenadaslugaresculturale.xlsx")
+    print("checkpint 1")
+    # Read the values of the file in the dataframe
+    cines = pd.DataFrame(excel_data, columns=['Nombre','Link Name']).head(10)
+    # Print the content
+    
+    print(list(cines["Link Name"]))
+    return list(cines["Link Name"])
 
-test()
+GetMovieTheaters()
